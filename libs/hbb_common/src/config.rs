95e4e1d8a9fe5d32ed709fe73a726accd6d46955
qqsq01 <61352826+qqsq01@users.noreply.github.com>
2024-09-26 12:04:45 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.qqsqkiss.eu.org"];
+pub const PUBLIC_RS_PUB_KEY: &str = "bw3ALRiQFr0umm36XPUEWGGbEIE8Md2gqkx3oRORLY4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
